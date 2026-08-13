56542a636cba847e92f81d83db5452407f4b6979
W1lly2 <guillermojimenez385@gmail.com>
2026-08-13 11:33:32 -0600

    migracion de archivos

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,60 @@
+import logo from '../assets/dev_logo.png'
+import profileIcon from '../assets/profile_icon.svg'
+import './Dashboard.css'
+import MainLayout from '../layout/MainLayout';
+
+function Dashboard(){
+    return(
+        <div className="dashboard-page">
+
+            <MainLayout>
+                <main className='dashboard-content'>
+                    <section className='task-table'>
+                        <div className='table-toolbar'>
+                            <input className='toolbar-search' type='text' placeholder='Buscar tarea'/>
+
+                            <select className='toolbar-select'>
+                                <option>Todas las prioridades</option>
+                                <option>Alta</option>
+                                <option>Media</option>
+                                <option>Baja</option>
+                            </select>
+                            <button className='toolbar-button'>Crear tarea</button>
+                        </div>
+
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Tarea</th>
+                                    <th>Prioridad</th>
+                                    <th>Estado</th>
+                                    <th>Fecha</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Crear Login</td>
+                                    <td>Alta</td>
+                                    <td>En progreso</td>
+                                    <td>31/07/2026</td>
+                                </tr>
+
+                                <tr>
+                                    <td>2</td>
+                                    <td>Diseñar Dashboard</td>
+                                    <td>Media</td>
+                                    <td>Pendiente</td>
+                                    <td>05/08/2026</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </section>
+                </main> 
+            </MainLayout>
+        </div>
+    );
+}
+
+export default Dashboard;
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,26 @@
+import './Login.css'
+import { useNavigate } from 'react-router-dom';
+import logo from '../assets/dev_logo.png';
+
+function Login(){
+    const navigate = useNavigate();
+    function handleLogin(){
+        navigate("/dashboard")
+    };
+    return(
+        <header className="login-page">
+            <form className="login-form">
+                <img src={logo} alt="DevBoard" className='login-logo' />
+                <label className="login-label">User</label>
+                <input className='login-input' type="text" placeholder='User'/>
+
+                <label className="login-label">Password</label>
+                <input className='login-input' type="password" placeholder='Password'/>
+
+                <button type='button' className='login-button' onClick={handleLogin}>Login</button>
+            </form>
+        </header>
+    );
+}
+
+export default Login
